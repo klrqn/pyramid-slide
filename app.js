0d7da42532de455ccb59a1b02568d1b7f37aa078
john doe <scottbruceburton@gmail.com>
2017-03-13 20:09:59 -0400

    basically there, need to css and pyramid is aligned

--- app.js
+++ app.js
@@ -1,50 +1,45 @@
 console.log("sanity check")
 
-var selectID = document.getElementById("selectID");
-var symbolValue = selectID.options[selectID.selectedIndex].value;
+var symbol = document.getElementById("symbolType");
+var height = document.getElementById("sliderRange");
+var output = document.getElementById("heightText");
 
-var heightElem = document.getElementById("pyramidRange").value;
-
-
-console.log(symbolValue);
-console.log(heightElem);
-
+// dispaly initial height and draw pyramid
+height.innerHTML = height.value;
+drawPyramid(parseInt(height.value), symbol.value);
 
 /**
  * drawPyramid
  *
  * Renders, in the HTML document, a Mario pyramid of the specified height
  */
-function drawPyramid(heightElem) {
+function drawPyramid(height, symbol) {
     // first, clear the old content
-    document.getElementById("output").innerHTML = "";
+    document.getElementById("pyramid").innerHTML = "";
+
+    var symbol = document.getElementById("symbolType");
+    var height = document.getElementById("sliderRange");
+
+    heightText.innerHTML = height.value;
 
     // for each row....
-    for (var row = 0; row < heightElem; row++) {
+    for (var row = 0; row < height.value; row++) {
         // figure out number of bricks and spaces
         var numBricks = row + 2;
-        var numSpaces = heightElem - row - 1;
+        var numSpaces = height - row - 1;
         // build up a string for this row
         var rowStr = "";
         for (var i = 0; i < numSpaces; i++) {
-            var spaceChar = "&nbsp"; // this is the HTML encoding for a space " "
+            var spaceChar = "WORKDAMNIT"; // this is the HTML encoding for a space " " "&nbsp"
             rowStr += spaceChar;
         }
         for (var i = 0; i < numBricks; i++) {
-            rowStr += symbolValue;
+            rowStr += symbol.value;
         }
 
         // make a <p> element for this row, and insert it into the #pyramid container
         rowElem = document.createElement("p");
         rowElem.innerHTML = rowStr;
-        document.getElementById("output").appendChild(rowElem);
+        document.getElementById("pyramid").appendChild(rowElem);
     }
 }
-
-
-
-
-// function myFunction() {
-//     var x = document.getElementById("mySelect").value;
-//     document.getElementById("demo").innerHTML = "You selected: " + x;
-// }
